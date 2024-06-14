711e7c50fa93b2423498de703ec2f3d27d8abcb7
yy7115 <yy7115@arcvideo.com>
2024-06-14 17:06:13 +0800

    更新mac编译脚本

--- AudioFilter/makefile/mac/installFilter.sh
+++ AudioFilter/makefile/mac/installFilter.sh
@@ -4,4 +4,4 @@ make clean
 make
 
 sudo cp -rf ../../src/dhfilter/FilterEngine.h /usr/local/include/FilterEngine/
-sudo cp -rf ../../output/mac-m2/libdhfilter.a /usr/local/lib/
+#sudo cp -rf ../../output/mac-m2/libdhfilter.a /usr/local/lib/
